8724622d350d1c7af04307323265d6f8011cc1a0
zhihaofans <zhihaofans@hotmail.com>
2021-05-31 09:26:30 +0800

    test

--- Core.js/kernel.js
+++ Core.js/kernel.js
@@ -2,9 +2,16 @@ const __VERSION__ = 1;
 class Kernel {
   constructor(name) {
     this.NAME = name;
+    this.REG_CORE_MOD_LIST = [];
   }
-  registerCoreMod() {}
+  registerCoreMod(mod_info) {
+    this.REG_CORE_MOD_LIST.push(mod_info);
+  }
+pushCoreModListView(){
+  
+}
 }
 module.exports = {
-  __VERSION__
+  __VERSION__,
+  Kernel
 };
