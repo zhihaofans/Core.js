948bbbca6293b6b946112ed8dce95acf39bb882e
zhihaofans <zhihaofans@hotmail.com>
2021-06-03 11:47:17 +0800

    a little update

---
+++ Core.js/default.js
@@ -0,0 +1,14 @@
+const device = {
+    type: $device.isIpad ? "ipad" : "iphone",
+    ios: $device.info.version
+  },
+  corejs = {
+    title: "Core.js",
+    version: "0.0.1"
+  },
+  http = {
+    user_agent: `${corejs.title}/${corejs.version} (${device.type}; iOS ${device.ios}; Scale/2.00)`
+  };
+module.exports = {
+  http
+};
--- Core.js/kernel.js
+++ Core.js/kernel.js
@@ -5,14 +5,35 @@ class Kernel {
     this.NAME = name;
     this.REG_CORE_MOD_LIST = [];
   }
-  errorLog(id, msg) {
-    $console.error(`${id}:${msg}`);
+  error(id, msg) {
+    let new_msg = msg ?? id,
+      result = "";
+    switch (typeof new_msg) {
+      case "object":
+        new_msg = JSON.stringify(new_msg);
+        break;
+    }
+    result = msg ? `${id}:${new_msg}` : new_msg;
+    $console.error(result);
+  }
+  l10n(l10nRes) {
+    const result = {};
+    Object.keys(l10nRes).map(key => {
+      const thisItem = l10nRes[key];
+      Object.keys(thisItem).map(lang => {
+        if (!result[lang]) {
+          result[lang] = {};
+        }
+        result[lang][key] = thisItem[lang];
+      });
+    });
+    $app.strings = result;
   }
   registerCoreMod(ModCore) {
     if (typeof ModCore.run === "function") {
       this.REG_CORE_MOD_LIST.push(ModCore);
     } else {
-      this.errorLog("registerCoreMod","ModCore.run ≠ function")
+      this.errorLog("registerCoreMod", "ModCore.run ≠ function");
     }
   }
   pushCoreModListView() {}
