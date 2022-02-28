2f054f756ee74520c93a6942f4186112b78448c2
zhihaofans <zhihaofans@hotmail.com>
2022-02-28 09:38:17 +0800

    新增未完成的 ModLoader，移除废弃代码

--- Core.js/core.js
+++ Core.js/core.js
@@ -1,5 +1,6 @@
 const CORE_VERSION = 3,
-  { Result } = require("./object");
+  object = require("./object"),
+  $ = require("./$");
 class Core {
   constructor({
     kernel,
@@ -15,7 +16,7 @@ class Core {
     this.Kernel = kernel;
     this.Storage = require("./storage");
     this.Http = require("./lib").Http;
-    this.$ = require("./$");
+    this.$ = $;
     this.CORE_INFO = {
       ID: modId,
       NAME: modName,
@@ -65,72 +66,32 @@ class Core {
     return this.SQLITE.setSimpleData(this.DATABASE_ID, key, value);
   }
 }
-
-class CoreChecker {
-  constructor(mod_dir) {
-    this.MOD_DIR = mod_dir;
+class ModLoader {
+  constructor({ modDir }) {
+    this.MOD_DIR = modDir;
+    this.modList = { id: [], mods: {} };
   }
-  runMod(mod) {
-    let fileName = `${this.MOD_DIR}${mod.FILE_NAME}`;
-    if (!fileName.endsWith(".js")) {
-      fileName += ".js";
-    }
-    if ($file.exists(fileName)) {
-      if ($file.isDirectory(fileName)) {
-        $ui.error("这是目录");
+  addMod(fileName) {
+    const filePath = this.MOD_DIR + fileName;
+    if (fileName.length > 0 && $.file.isFileExist(filePath)) {
+      const thisMod = require(filePath),
+        modId = thisMod.CORE_INFO.ID;
+      if (this.modList.id.indexOf(modId) < 0) {
+        this.modList.id.push(modId);
+        this.modList.mods[modId] = thisMod;
       } else {
-        const coreMod = require(fileName),
-          modRun = coreMod.run,
-          _SUPPORT_COREJS_ = coreMod._SUPPORT_COREJS_;
-        if (typeof modRun === "function" && _SUPPORT_COREJS_ === 1) {
-          try {
-            const runResult = modRun();
-            if (runResult.success === true) {
-              $console.info(`(core.js)Mod加载完毕:${mod.MOD_NAME}`);
-            } else {
-              $ui.alert({
-                title: `Core.js加载[${mod.MOD_NAME}]失败(${runResult.code})`,
-                message: runResult.error_message,
-                actions: [
-                  {
-                    title: "OK",
-                    disabled: false, // Optional
-                    handler: function () {}
-                  }
-                ]
-              });
-            }
-          } catch (error) {
-            $ui.alert({
-              title: `${mod.MOD_NAME}加载失败(catch)`,
-              message: error.message,
-              actions: [
-                {
-                  title: "OK",
-                  disabled: false, // Optional
-                  handler: function () {}
-                }
-              ]
-            });
-          }
-        } else {
-          $ui.alert({
-            title: `该Mod不支持core.js`,
-            message: "请用旧版加载模式",
-            actions: [
-              {
-                title: "OK",
-                disabled: false, // Optional
-                handler: function () {
-                  coreMod.init();
-                }
-              }
-            ]
-          });
-        }
+        throw new object.UserException({
+          name: "重复",
+          message: "重复添加mod",
+          source: "developer"
+        });
       }
     } else {
-      $ui.error("不存在该文件");
+      throw new object.UserException({
+        name: "404",
+        message: "找不到该mod",
+        source: "developer"
+      });
     }
   }
 }
@@ -138,25 +99,5 @@ class CoreChecker {
 module.exports = {
   __VERSION__: CORE_VERSION,
   Core,
-  Result,
-  CoreChecker,
-  // <Core.js use guide>
-  _SUPPORT_COREJS_: 1,
-  run: () => {
-    const _core = new Core(),
-      ver = _core.checkCoreVersion();
-    if (ver == 0) {
-      _core.initView();
-      return new _core.Result({
-        success: true,
-        code: 0
-      });
-    } else {
-      return new _core.Result({
-        success: false,
-        code: 1,
-        error_message: `need update core.js(${ver})`
-      });
-    }
-  }
+  ModLoader
 };
