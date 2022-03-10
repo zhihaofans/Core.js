c7bba8fe3774f54932df499c6340481debde7b0d
zhihaofans <zhihaofans@hotmail.com>
2022-03-10 16:17:39 +0800

    新增AppLog

--- Core.js/uuid.js
+++ Core.js/uuid.js
@@ -1,13 +1,18 @@
 class UserUUID {
-  constructor() {
+  constructor(appKernel) {
+    this.AppKernel = appKernel;
     this.DEVELOPER_DIR = "shared://zhihaofans/";
     this.DATA_DIR = this.DEVELOPER_DIR + "Core.js/";
     this.KEYCHAIN_DOMAIN = "zhihaofans.corejs";
-    this.init();
-  }
-  init() {
     $file.mkdir(this.DEVELOPER_DIR);
     $file.mkdir(this.DATA_DIR);
+    this.init();
+    this.AppLog = new AppLog({
+      appKernel: this.AppKernel,
+      dataDir: this.DATA_DIR
+    });
+  }
+  init() {
     this.UUID = this.getDeviceUUID();
     this.saveUserData();
   }
@@ -23,10 +28,31 @@ class UserUUID {
       $keychain.get(this.KEYCHAIN_DOMAIN, "uuid") ||
       $file.read(this.DATA_DIR + "uuid") ||
       $text.uuid;
-
     return UUID;
   }
 }
+class AppLog {
+  constructor({ appKernel, dataDir }) {
+    this.AppKernel = appKernel;
+    this.DATA_DIR = dataDir;
+    this.setLastApp({
+      message: `logtime:${new Date().getTime()}`
+    });
+  }
+  setLastApp({ message }) {
+    const lastAppLog = {
+      appId: this.AppKernel.AppInfo.id,
+      appName: this.AppKernel.AppInfo.name,
+      timestamp: this.AppKernel.START_TIME,
+      message
+    };
+    $file.write({
+      data: $data({ string: JSON.stringify(lastAppLog) }),
+      path: this.DATA_DIR + "applog.json"
+    });
+  }
+}
+
 module.exports = {
   UserUUID
 };
